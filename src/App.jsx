cad155c0e0beddff6e18edf2e0a7ea80b336cea6
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-10-18 14:47:46 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Stock Tracker
+      </h1>
+      <div className="flex justify-center">
+        <div className="w-1/2">
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Symbol</th>
+                <th className="px-4 py-2">Price</th>
+                <th className="px-4 py-2">Change</th>
+                <th className="px-4 py-2">Change %</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border px-4 py-2">AAPL</td>
+                <td className="border px-4 py-2">145.86</td>
+                <td className="border px-4 py-2">+0.22</td>
+                <td className="border px-4 py-2">+0.15%</td>
+              </tr>
+              <tr>
+                <td className="border px-4 py-2">GOOGL</td>
+                <td className="border px-4 py-2">2,763.82</td>
+                <td className="border px-4 py-2">-4.18</td>
+                <td className="border px-4 py-2">-0.15%</td>
+              </tr>
+              <tr>
+                <td className="border px-4 py-2">MSFT</td>
+                <td className="border px-4 py-2">277.65</td>
+                <td className="border px-4 py-2">+0.15</td>
+                <td className="border px-4 py-2">+0.05%</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
